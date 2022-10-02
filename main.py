4b93aca2dc3f3211287b878d35a23d5f8f235de0
fatemakotha <fatema.alam.kotha@gmail.com>
2022-10-02 16:14:09 +0600

    Created another object named user_2

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 class User: #creates a class named user
     #initialize attributes.
     def __int__(self): #In order to be able to add the same attributes to different objects like user_1, user+2 etc we used this
+        print(f"new user being created")
 
 
 
@@ -11,3 +12,7 @@ user_1.id = "001" #adding an attribute for the class named User
 user_1.username = "Kotha" #adding another attribute for the class named User
 
 print(user_1.username) #prints: Kotha
+
+user_2 = User() #creates object named user_1 using class named User
+user_2.id = "002" #adding an attribute for the class named User
+user_2.username = "Cutu" #adding another attribute for the class named User
