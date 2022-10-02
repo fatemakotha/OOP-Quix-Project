26e3eb9f557df27c17d55983044d8d8b8b5b3cee
fatemakotha <fatema.alam.kotha@gmail.com>
2022-10-02 16:08:18 +0600

    Added another attribute for the class named User

--- main.py
+++ main.py
@@ -5,3 +5,6 @@ class User: #creates a class named user
 
 user_1 = User() #creates object named user_1 using class named User
 user_1.id = "001" #adding an attribute for the class named User
+user_1.username = "Kotha" #adding another attribute for the class named User
+
+print(user_1.username)
