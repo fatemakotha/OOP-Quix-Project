f978c6e548e08d2369298878eeaafdfad48015a3
fatemakotha <fatema.alam.kotha@gmail.com>
2022-10-02 19:45:58 +0600

    Commented

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 #Create a class
 class User: #creates a class named user
     #initialize attributes.
-    def __init__(self, user_id, username): #In order to be able to add the same attributes to different objects like user_1, user+2 etc we used this
+    def __init__(self, user_id, username): #In order to be able to add the same attributes to different objects like user_1, user+2 etc we use this
         print("new user being created...") #everytime that we create a new user, this print statement will be triggered
         self.id = user_id #here name of parameter is NOT equal to the attribute
         self.username = username #here name of parameter is equal to the attribute
@@ -10,6 +10,6 @@ class User: #creates a class named user
 
 
 user_1 = User("001", "Kotha",)
-print(user_1.username)
+print(user_1.id) #prints "001"
 user_2 = User("002", "Cutu")
-print(user_2.username)
+print(user_2.username) #prints: "Cutu
