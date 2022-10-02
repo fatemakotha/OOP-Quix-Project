0c73f1bbb9a61a0b10ac64239a0b963806fad9c2
fatemakotha <fatema.alam.kotha@gmail.com>
2022-10-02 16:04:25 +0600

    Used pass keyword to leave a class empty

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 #Create a class
 class User: #creates a class named user
+    pass
 
 
-user_1 = User #creates object named user_1 using class named User
+user_1 = User() #creates object named user_1 using class named User
